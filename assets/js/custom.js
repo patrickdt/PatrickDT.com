bb3804bbdff9e8a702c0e18be1e4c24ad1302789
Patrick Tirtapraja <patrick.tpraja@gmail.com>
2019-09-15 06:50:20 -0400

    added footer, contact, and navbar

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -6,3 +6,19 @@ $(document).ready(function() {
     new WOW().init();
 })
 
+// TODO: make an automatic carousel with the different logos of programming, design languages
+
+// Navigation
+//show/hide transparent black navigation
+$(function() {
+    $(window).scroll(function() {
+        // if scroll top is less than 50 pixels, hide the nav bar
+        if ($(this).scrollTop() < 50) {
+            $("nav").removeClass("web-top-nav");
+        }
+        else { // show
+            $("nav").addClass("web-top-nav");
+
+        }
+    });
+});
