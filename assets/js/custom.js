4623878d0aa36e8f4c5f42dd1d008fe5c27b1621
Patrick Tirtapraja <patrick.tpraja@gmail.com>
2019-09-15 22:04:34 -0400

    added smooth scrolling, easing, working on carousel

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,7 +1,7 @@
 /*==================
 Experiences
 ==============*/
-$(document).ready(function() {
+$(document).ready(function () {
     // animate on scroll
     new WOW().init();
 })
@@ -9,16 +9,34 @@ $(document).ready(function() {
 // TODO: make an automatic carousel with the different logos of programming, design languages
 
 // Navigation
-//show/hide transparent black navigation
-$(function() {
-    $(window).scroll(function() {
+// Show/hide transparent black navigation
+$(function () {
+    $(window).scroll(function () {
         // if scroll top is less than 50 pixels, hide the nav bar
         if ($(this).scrollTop() < 50) {
             $("nav").removeClass("web-top-nav");
+            $("#back-to-top").fadeOut();
         }
         else { // show
             $("nav").addClass("web-top-nav");
+            $("#back-to-top").fadeIn();
 
+        }
+    });
+});
+
+
+$(function () {
+    $('a.smooth-scroll').on('click', function (e) {
+        // console.log(this.hash);  // prints id #home, #experiences, #about, #projects, etc
+        if (this.hash != '') {
+            e.preventDefault();
+
+            const hash = this.hash;
+
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top - 55
+            }, 1250, 'easeInOutExpo');
         }
     });
 });
--- assets/js/html.js
+++ assets/js/html.js
@@ -22,43 +22,6 @@
 			}, 100);
 		});
 
-	// Mobile?
-		if (browser.mobile)
-			$body.addClass('is-mobile');
-		else {
-
-			breakpoints.on('>medium', function() {
-				$body.removeClass('is-mobile');
-			});
-
-			breakpoints.on('<=medium', function() {
-				$body.addClass('is-mobile');
-			});
-
-		}
-
-	// Scrolly.
-		$('.scrolly')
-			.scrolly({
-				speed: 1500,
-				offset: $header.outerHeight()
-			});
-
-	// Menu.
-		$('#menu')
-			.append('<a href="#menu" class="close"></a>')
-			.appendTo($body)
-			.panel({
-				delay: 500,
-				hideOnClick: true,
-				hideOnSwipe: true,
-				resetScroll: true,
-				resetForms: true,
-				side: 'right',
-				target: $body,
-				visibleClass: 'is-menu-visible'
-			});
-
 	// Header.
 		if ($banner.length > 0
 		&&	$header.hasClass('alt')) {
